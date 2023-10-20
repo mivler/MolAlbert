822742da90810c2f7315a6e457839d20a8017a37
Matthew Ivler <mnivler@gmail.com>
2023-10-20 22:21:19 +0000

    Reformat split_SMILES.py to work with utils function

--- scripts/split_SMILES.py
+++ scripts/split_SMILES.py
@@ -8,7 +8,7 @@ authors: mivler
 import pandas as pd
 import argparse
 import os
-from sklearn.model_selection import train_test_split
+from utils.data_preparation import train_valid_test_split
 
 
 def main():
@@ -28,14 +28,7 @@ def main():
     df = pd.read_csv(args.infile)
     X = df["smiles"]
 
-    # Set up proportions of validation and test sets
-    test_size = args.test_perc
-    valid_size = args.valid_perc / (args.valid_perc + args.train_perc)
-
-    # Split dataset into test set and all other examples
-    x_train_val, x_test = train_test_split(X, test_size=test_size, random_state=args.random_state)
-    # Split remaining examples into train set and validation set
-    x_train, x_val = train_test_split(x_train_val, test_size=valid_size, random_state=args.random_state)
+    train_set, valid_set, test_set = train_valid_test_split(X, train_size=args.train_perc, valid_size=args.valid_perc, test_size=args.test_perc, random_state=args.random_state)
     
     # Grab original filename of csv file (without file extension)
     original_filename = ".".join(args.infile.split(os.sep)[-1].split(".")[:-1])
@@ -43,9 +36,9 @@ def main():
     out_name = os.path.join(args.out_dir, original_filename)
 
     # Saves each set split into a properly labeled file using the at location <out_dir>/<original_file_name>_<split_partition>.csv
-    pd.Series(x_train).to_csv(os.path.join(f"{out_name}_train.csv"), index=False, header=False)
-    pd.Series(x_val).to_csv(os.path.join(f"{out_name}_valid.csv"), index=False, header=False)
-    pd.Series(x_test).to_csv(os.path.join(f"{out_name}_test.csv"), index=False, header=False)
+    pd.Series(train_set[0]).to_csv(os.path.join(f"{out_name}_train.csv"), index=False, header=False)
+    pd.Series(valid_set[0]).to_csv(os.path.join(f"{out_name}_valid.csv"), index=False, header=False)
+    pd.Series(test_set[0]).to_csv(os.path.join(f"{out_name}_test.csv"), index=False, header=False)
 
 
 if __name__ == "__main__":
